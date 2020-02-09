187e0a4cdd253b09b6a45dd359b46ef4ef9a6998
Krystian <krupenz97@gmail.com>
2020-02-09 12:30:24 +0100

    stworzenie tworzenie prostego template index

--- Rezerwacje/RezerwacjeApp/admin.py
+++ Rezerwacje/RezerwacjeApp/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(Rezerwacje)
+admin.site.register(Trasy)
+admin.site.register(Pasazerowie)
+admin.site.register(Siedzenia)
+admin.site.register(Wymagania)
+admin.site.register(Lotniska)
+admin.site.register(MiejscaSpecjalne)
+admin.site.register(Loty)
+admin.site.register(Samoloty)
+
+admin.site.site_header = "Adison Airlines"
--- Rezerwacje/RezerwacjeApp/models.py
+++ Rezerwacje/RezerwacjeApp/models.py
@@ -6,24 +6,34 @@ class Pasazerowie(models.Model):
     imie = models.CharField(max_length=20)
     nazwisko = models.CharField(max_length=20)
 
+    def __str__(self):
+        return self.imie + self.nazwisko
 
 class Rezerwacje(models.Model):
     pasazer = models.ForeignKey(Pasazerowie, on_delete=models.CASCADE)
     data = models.DateTimeField()
 
+    def __str__(self):
+        return str(self.id) + "- " + str(self.pasazer) + " data rezerwacji: " + str(self.data)[:19]
 
 class Lotniska(models.Model):
     nazwa = models.CharField(max_length=20)
 
+    def __str__(self):
+        return self.nazwa
 
 class Trasy(models.Model):
     miejsce_odlotu = models.ForeignKey(Lotniska, related_name="miejsce_odlotu", on_delete=models.CASCADE)
     miejsce_przylotu = models.ForeignKey(Lotniska, related_name="miejsce_przylotu", on_delete=models.CASCADE)
 
+    def __str__(self):
+        return str(self.miejsce_odlotu) + " --> " + str(self.miejsce_przylotu)
 
 class Samoloty(models.Model):
     ilosc_miejsc = models.IntegerField()
 
+    def __str__(self):
+        return "id=" + str(self.id)
 
 class Loty(models.Model):
     data_odlotu = models.DateTimeField()
@@ -32,6 +42,9 @@ class Loty(models.Model):
     samolot = models.ForeignKey(Samoloty, on_delete=models.CASCADE)
     numer_lotu = models.CharField(max_length=10)
 
+    def __str__(self):
+        return self.numer_lotu + " " + str(self.trasa) + " " + str(self.data_odlotu)[:19]
+
 
 class Siedzenia(models.Model):
     samolot = models.ForeignKey(Samoloty, on_delete=models.CASCADE)
@@ -39,11 +52,20 @@ class Siedzenia(models.Model):
     pasazer = models.ForeignKey(Pasazerowie, on_delete=models.CASCADE)
     miejsce = models.IntegerField()
 
+    def __str__(self):
+        return self.numer_lotu + " " + str(self.miejsce)
+
 
 class Wymagania(models.Model):
     nazwa = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.nazwa
 
 
 class MiejscaSpecjalne(models.Model):
     wymaganie = models.ForeignKey(Wymagania, on_delete=models.CASCADE)
     siedzenie = models.ForeignKey(Siedzenia, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.siedzenie + self.wymaganie
